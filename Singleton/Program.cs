00ec6885a19c356240b93396319364c155a46ef5
Berkay Bozkurt <BozkurtBerkay@hotmail.com>
2020-11-29 17:25:08 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonDeneme singleton = SingletonDeneme.GetSingleton();
+            singleton.title = "Canberk";
+            Console.WriteLine(singleton.id.ToString()+" "+singleton.title);
+
+        }
+    }
+} 
---
+++ Singleton/SingletonDeneme.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class SingletonDeneme
+    {
+        private static SingletonDeneme singletonDeneme;
+        private static Guid _id;
+        private static object _lockObject = new object();
+        private string _title;
+
+        private SingletonDeneme(Guid id, string title)
+        {
+            _title = title;
+            _id = id;
+        }
+
+        public Guid id
+        {
+            get { return _id; }
+        } 
+        
+        public string title
+        {
+            get { return _title; }
+            set { _title = title; }
+        }
+         
+
+        public static SingletonDeneme GetSingleton()
+        {
+            if (singletonDeneme == null)
+            {
+                lock (_lockObject) { 
+                    if(singletonDeneme == null) { 
+                        singletonDeneme = new SingletonDeneme(Guid.NewGuid(),title:"Berkay");
+                    }
+                }
+            }    
+            return singletonDeneme;
+        }
+    }
+}
